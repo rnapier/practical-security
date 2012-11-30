6ad8dd7fcc3c1f34381999023a9fd23be6c868d4
Rob Napier <robnapier@gmail.com>
2012-11-29 21:49:00 -0500

    Re-add nested git

---
+++ SelfCert/SelfCert/Connection.h
@@ -0,0 +1,13 @@
+//
+//  Connection.h
+//  SelfCert
+//
+//  Created by Rob Napier on 11/25/12.
+//  Copyright (c) 2012 Rob Napier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Connection : NSObject
+
+@end
