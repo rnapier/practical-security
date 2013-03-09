81ebc4632aedc4f6a5095f3a5c93e3346aaf72b9
Rob Napier <robnapier@gmail.com>
2013-03-09 14:58:28 -0500

    Code cleanup in FileProtection.

--- FileProtection/FileProtection/FPAppDelegate.m
+++ FileProtection/FileProtection/FPAppDelegate.m
@@ -9,19 +9,113 @@
 #import "FPAppDelegate.h"
 
 
-NSString *DocumentsDirectory() {
-  return NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
-}
-
-NSString *PathForName(NSString *name) {
-  return [DocumentsDirectory() stringByAppendingPathComponent:name];
-}
-
 @interface FPAppDelegate ()
 @property (nonatomic) UIBackgroundTaskIdentifier backgroundTask;
 @end
 
 @implementation FPAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+  /*
+   Normally it would be a good idea to call -upgradeFilesInDirectory:error: here, but
+   we're deleting everything in the documents directory.
+   */
+  [self cleanDocuments];
+
+  /* Create a protected file */
+  [self createProtectedFileWithOptions:NSDataWritingFileProtectionComplete
+                                  name:@"complete"
+                         shouldSucceed:YES];
+
+  NSLog(@"Lock device now.");
+
+  return YES;
+}
+
+/*
+ Creating a protected file
+ */
+
+- (void)createProtectedFileWithOptions:(enum NSDataWritingOptions)options
+                                  name:(NSString *)name
+                         shouldSucceed:(BOOL)shouldSucceed {
+  NSData *data = [@"This is some protected data" dataUsingEncoding:NSUTF8StringEncoding];
+
+  NSLog(@"Creating '%@'. Should%@ succeed.", name, shouldSucceed ? @"" : @" not");
+  NSError *error;
+  [self logResult:[data writeToFile:PathForName(name)
+                            options:options
+                              error:&error]
+   expectedResult:shouldSucceed];
+}
+
+/*
+ Called when protected data is going to be encrypted "soon." This is generally called
+ as soon as the device is locked, but the data isn't really encrypted for 10 seconds.
+ */
+- (void)applicationProtectedDataWillBecomeUnavailable:(UIApplication *)application {
+  double delayInSeconds = 11.0;
+  NSLog(@"Encrypting. Waiting %.0f seconds", delayInSeconds);
+
+  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW,
+                                          (uint64_t)(delayInSeconds * NSEC_PER_SEC));
+  dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
+    NSLog(@"Reading after encryption");
+
+    /* Demonstrate failure when reading a protected file while we're locked */
+    [self readFileForName:@"complete" shouldSucceed:NO];
+
+    /* Demonstrate failure trying to create a protected file while we're locked */
+    [self createProtectedFileWithOptions:NSDataWritingFileProtectionComplete
+                                    name:@"complete-after-encryption"
+                           shouldSucceed:NO];
+
+    /* Demonstrate creating a protected file while locked using "...UnlessOpen" */
+    [self createProtectedFileWithOptions:NSDataWritingFileProtectionCompleteUnlessOpen
+                                    name:@"complete-unless-open"
+                           shouldSucceed:YES];
+
+    [application endBackgroundTask:self.backgroundTask];
+    self.backgroundTask = UIBackgroundTaskInvalid;
+  });
+}
+
+/*
+ Called when the device is unlocked and data is unencrypted. This is a good point to
+ upgrade the encryption of any files you created while locked.
+ */
+- (void)applicationProtectedDataDidBecomeAvailable:(UIApplication *)application {
+  NSLog(@"Upgrading files");
+  NSError *error = nil;
+  [self upgradeFilesInDirectory:DocumentsDirectory() error:&error];
+  [self logResult:(error == nil) expectedResult:YES];
+}
+
+
+/* 
+ Simple implementation of upgrading the file protections to "complete." This 
+ implementation upgrades everything in the Documents directory.
+ */
+- (void)upgradeFilesInDirectory:(NSString *)dir
+                          error:(NSError **)error
+{
+  NSFileManager *fm = [NSFileManager defaultManager];
+  NSDirectoryEnumerator *dirEnum = [fm enumeratorAtPath:dir];
+  for (NSString *path in dirEnum) {
+    NSDictionary *attrs = [dirEnum fileAttributes];
+    if (![[attrs objectForKey:NSFileProtectionKey] isEqual:NSFileProtectionComplete]) {
+      [fm setAttributes:@{NSFileProtectionKey : NSFileProtectionComplete}
+           ofItemAtPath:[DocumentsDirectory() stringByAppendingPathComponent:path]
+                  error:error];
+    }
+  }
+}
+
+/**********************************************************************/
+/*
+ Helper methods. Things below here have fairly obvious implementations.
+ */
 
 - (void)readFileForName:(NSString *)name shouldSucceed:(BOOL)shouldSucceed
 {
@@ -45,29 +139,6 @@ NSString *PathForName(NSString *name) {
   }
 }
 
-// Create a protected file.
-- (void)createProtectedFileWithOptions:(enum NSDataWritingOptions)options name:(NSString *)name shouldSucceed:(BOOL)shouldSucceed
-{
-  NSData *data = [@"This is some protected data" dataUsingEncoding:NSUTF8StringEncoding];
-
-  NSLog(@"Creating '%@'. Should%@ succeed.", name, shouldSucceed ? @"" : @" not");
-  NSError *error;
-  [self logResult:[data writeToFile:PathForName(name)
-                            options:options
-                              error:&error]
-   expectedResult:shouldSucceed];
-}
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  [self cleanDocuments];
-  [self createProtectedFileWithOptions:NSDataWritingFileProtectionComplete name:@"complete" shouldSucceed:YES];
-
-  NSLog(@"Lock device now");
-
-  return YES;
-}
-
 - (void)cleanDocuments
 {
   NSFileManager *fm = [NSFileManager defaultManager];
@@ -89,45 +160,12 @@ NSString *PathForName(NSString *name) {
   NSLog(@"Entering background");
 }
 
-- (void)applicationProtectedDataDidBecomeAvailable:(UIApplication *)application
-{
-  NSLog(@"Upgrading files");
-  NSError *error = nil;
-  [self upgradeFilesInDirectory:DocumentsDirectory() error:&error];
-  [self logResult:(error == nil) expectedResult:YES];
+NSString *DocumentsDirectory() {
+  return NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
 }
 
-- (void)applicationProtectedDataWillBecomeUnavailable:(UIApplication *)application
-{
-  double delayInSeconds = 11.0;
-  NSLog(@"Encrypting. Waiting %.0f seconds", delayInSeconds);
-
-  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (uint64_t)(delayInSeconds * NSEC_PER_SEC));
-  dispatch_after(popTime, dispatch_get_main_queue(), ^(void) {
-    NSLog(@"Reading after encryption");
-    [self readFileForName:@"complete" shouldSucceed:NO];
-
-    [self createProtectedFileWithOptions:NSDataWritingFileProtectionComplete name:@"complete-after-encryption" shouldSucceed:NO];
-    [self createProtectedFileWithOptions:NSDataWritingFileProtectionCompleteUnlessOpen name:@"complete-unless-open" shouldSucceed:YES];
-
-    [application endBackgroundTask:self.backgroundTask];
-    self.backgroundTask = UIBackgroundTaskInvalid;
-  });
-}
-
-- (void)upgradeFilesInDirectory:(NSString *)dir
-                          error:(NSError **)error
-{
-  NSFileManager *fm = [NSFileManager defaultManager];
-  NSDirectoryEnumerator *dirEnum = [fm enumeratorAtPath:dir];
-  for (NSString *path in dirEnum) {
-    NSDictionary *attrs = [dirEnum fileAttributes];
-    if (![[attrs objectForKey:NSFileProtectionKey] isEqual:NSFileProtectionComplete]) {
-      [fm setAttributes:@{NSFileProtectionKey : NSFileProtectionComplete}
-           ofItemAtPath:[DocumentsDirectory() stringByAppendingPathComponent:path]
-                  error:error];
-    }
-  }
+NSString *PathForName(NSString *name) {
+  return [DocumentsDirectory() stringByAppendingPathComponent:name];
 }
 
 @end
